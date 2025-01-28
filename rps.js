17b72410816b5af7b2f5a6e10eebe48afa6f2ddb
Abood <Aboodabogus@gmail.com>
2025-01-28 18:02:15 +0300

    added human choice input

--- rps.js
+++ rps.js
@@ -18,5 +18,23 @@ function getComputerChoice(){
     }
 }
 
+// Computer outputs as expected ✅
+// alert(getComputerChoice());
 
-alert(getComputerChoice());
+//Function getHumanChoice to get human input 
+function getHumanChoice(){
+   let  humanChoice = prompt("choose an option", "rock");
+   //must return a valid option either rock,paper,scissors
+   if ( humanChoice == "rock"){
+    return humanChoice;
+   }else if (humanChoice == "paper"){
+    return humanChoice;
+   }else if ( humanChoice == "scissors"){
+    return humanChoice;
+   }else {
+     alert("Please enter a valid option")
+   }
+
+}
+// Human input works as expected ✅
+// getHumanChoice()
